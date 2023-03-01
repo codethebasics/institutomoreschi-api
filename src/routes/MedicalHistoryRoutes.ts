487cb1e5c86592ad399d3ea354367b271ed6d0c3
codethebasics <codethebasics@gmail.com>
2023-03-01 19:42:49 -0300

    dev

--- src/routes/MedicalHistoryRoutes.ts
+++ src/routes/MedicalHistoryRoutes.ts
@@ -1,14 +1,50 @@
-import express from "express";
-import MedicalHistoryService from "../services/MedicalHistoryService";
+import express from "express"
+import MedicalHistoryService from "../services/MedicalHistoryService"
 
 const router = express.Router()
 const medicalHistoryService = new MedicalHistoryService()
 
 router
-    .route ('/')
-    .get   (async (req, res) => res.json(await medicalHistoryService.findAll()))
-    .post  (async (req, res) => res.json(await medicalHistoryService.create (req.body)))
-    .put   (async (req, res) => res.json(await medicalHistoryService.update (req.body)))
-    .delete(async (req, res) => res.json(await medicalHistoryService.remove (req.body)))
+  .route("/")
+  .get(async (req, res) => res.json(await medicalHistoryService.findAll()))
+  .post(async (req, res) =>
+    res.json(await medicalHistoryService.create(req.body))
+  )
+  .put(async (req, res) =>
+    res.json(await medicalHistoryService.update(req.body))
+  )
+  .delete(async (req, res) =>
+    res.json(await medicalHistoryService.remove(req.body))
+  )
 
-export default router;
+router
+  .route("/id/:id")
+  .get(async (req, res) => res.json(await findById(req, res)))
+
+router
+  .route("/patientId/:id")
+  .get(async (req, res) => res.json(await findByPatient(req, res)))
+
+async function findById(req: any, res: any): Promise<any> {
+  try {
+    const { id } = req.params
+    if (id) {
+      return await medicalHistoryService.findById(id)
+    }
+  } catch (e: any) {
+    res.status(500).json({ message: e.message })
+  }
+}
+
+async function findByPatient(req: any, res: any): Promise<any> {
+  try {
+    const { id } = req.params
+    if (id) {
+      return await medicalHistoryService.findByPatient(id)
+    }
+  } catch (e: any) {
+    res.status(500).json({ message: e.message })
+  }
+}
+
+export default router
--- src/services/MedicalHistoryService.ts
+++ src/services/MedicalHistoryService.ts
@@ -1,62 +1,93 @@
-import { MedicalHistoryCreateResponse, MedicalHistoryRemoveRequest, MedicalHistoryRemoveResponse, MedicalHistorySelectResponse, MedicalHistoryUpdateRequest, MedicalHistoryUpdateResponse } from "../interfaces/dto/medical-history/MedicalHistoryDTO";
-import medicalHistoryRepository from "../repository/MedicalHistoryRepository";
+import {
+  MedicalHistoryCreateResponse,
+  MedicalHistoryRemoveRequest,
+  MedicalHistoryRemoveResponse,
+  MedicalHistorySelectResponse,
+  MedicalHistoryUpdateRequest,
+  MedicalHistoryUpdateResponse,
+} from "../interfaces/dto/medical-history/MedicalHistoryDTO"
+import medicalHistoryRepository from "../repository/MedicalHistoryRepository"
 
 export default class MedicalHistoryService {
+  private medicalHistoryRepository: medicalHistoryRepository
 
-    private medicalHistoryRepository: medicalHistoryRepository
+  constructor() {
+    this.medicalHistoryRepository = new medicalHistoryRepository()
+  }
 
-    constructor () {    
-        this.medicalHistoryRepository = new medicalHistoryRepository()
+  async findAll(): Promise<MedicalHistorySelectResponse[]> {
+    try {
+      const response = await this.medicalHistoryRepository.findAll()
+      return response
+    } catch (e: any) {
+      console.error(e)
+      throw e
     }
+  }
 
-    async findAll(): Promise<MedicalHistorySelectResponse[]> {
-        try {
-            const response = await this.medicalHistoryRepository.findAll()
-            return response
-        } catch (e: any) {
-            console.error(e)
-            throw e
-        }
+  async findById(id: string): Promise<MedicalHistorySelectResponse> {
+    try {
+      const response = await this.medicalHistoryRepository.findById(id)
+      return response
+    } catch (e: any) {
+      console.error(e)
+      throw e
     }
+  }
 
-    async create(medicalHistory: {
-        patientId: string,
-        dentistId: string,
-        description: string
-    }): Promise<MedicalHistoryCreateResponse> {
-        try {
-            return await this.medicalHistoryRepository.save({
-                dentistId: medicalHistory.dentistId,
-                patientId: medicalHistory.patientId,
-                description: medicalHistory.description,
-            })
-        } catch (e: any) {
-            console.error(e)
-            throw e
-        }
+  async findByPatient(patient: any): Promise<MedicalHistorySelectResponse[]> {
+    try {
+      const response = await this.medicalHistoryRepository.findByPatient(
+        patient
+      )
+      return response
+    } catch (e: any) {
+      console.error(e)
+      throw e
     }
+  }
 
-    async update(medicalHistory: MedicalHistoryUpdateRequest): Promise<MedicalHistoryUpdateResponse> {
-        try {
-            return await this.medicalHistoryRepository.update({
-                id: medicalHistory.id,
-                description: medicalHistory.description,
-            })
-        } catch (e: any) {
-            console.error(e)
-            throw e
-        }
+  async create(medicalHistory: {
+    patientId: string
+    dentistId: string
+    description: string
+  }): Promise<MedicalHistoryCreateResponse> {
+    try {
+      return await this.medicalHistoryRepository.save({
+        dentistId: medicalHistory.dentistId,
+        patientId: medicalHistory.patientId,
+        description: medicalHistory.description,
+      })
+    } catch (e: any) {
+      console.error(e)
+      throw e
     }
+  }
 
-    async remove(medicalHistory: MedicalHistoryRemoveRequest): Promise<MedicalHistoryRemoveResponse> {
-        try {
-            return await this.medicalHistoryRepository.remove({
-                id: medicalHistory.id,
-            })
-        } catch (e: any) {
-            console.error(e)
-            throw e
-        }
+  async update(
+    medicalHistory: MedicalHistoryUpdateRequest
+  ): Promise<MedicalHistoryUpdateResponse> {
+    try {
+      return await this.medicalHistoryRepository.update({
+        id: medicalHistory.id,
+        description: medicalHistory.description,
+      })
+    } catch (e: any) {
+      console.error(e)
+      throw e
     }
-    
+  }
+
+  async remove(
+    medicalHistory: MedicalHistoryRemoveRequest
+  ): Promise<MedicalHistoryRemoveResponse> {
+    try {
+      return await this.medicalHistoryRepository.remove({
+        id: medicalHistory.id,
+      })
+    } catch (e: any) {
+      console.error(e)
+      throw e
+    }
+  }
 }
